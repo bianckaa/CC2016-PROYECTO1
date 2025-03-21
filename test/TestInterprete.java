2bf60d11bbc8394cce986f2d8c82b73248a692c0
bianckaa <bianckaraxon@gmail.com>
2025-03-20 23:52:30 -0600

    remove warning in testInterprete

--- test/TestInterprete.java
+++ test/TestInterprete.java
@@ -5,20 +5,19 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TestInterprete {
-    private DocumentControler controller;
     private VariableManagement<Object> variableManager;
     private Interprete interprete;
 
     @BeforeEach
     void setUp() {
-        controller = new DocumentControler("dummyPath");
+        new DocumentControler("dummyPath");
         variableManager = new VariableManagement<>();
         interprete = new Interprete("dummyPath");
     }
 
     @Test
     void testInterpretarConQuote() {
-        controller = new DocumentControler("dummyPath") {
+        new DocumentControler("dummyPath") {
             @Override
             public Stack<String> processLispCode() {
                 Stack<String> tokens = new Stack<>();
@@ -32,7 +31,7 @@ class TestInterprete {
 
     @Test
     void testInterpretarConSetq() {
-        controller = new DocumentControler("dummyPath") {
+        new DocumentControler("dummyPath") {
             @Override
             public Stack<String> processLispCode() {
                 Stack<String> tokens = new Stack<>();
@@ -50,8 +49,7 @@ class TestInterprete {
 
     @Test
     void testInterpretarConDefun() {
-        // Simulamos tokens para definir una función
-        controller = new DocumentControler("dummyPath") {
+        new DocumentControler("dummyPath") {
             @Override
             public Stack<String> processLispCode() {
                 Stack<String> tokens = new Stack<>();
